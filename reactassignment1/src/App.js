e736b379307109cf931fa09854fb6f81f4efe7f3
Lasantha Kumara <lasanthakumaraofficial@gmail.com>
2024-11-19 16:12:15 +0000

    Initiating Project with uploading Assets

---
+++ reactassignment1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <h1>Student Information Portal</h1>
+    </div>
+  );
+}
+
+export default App;
