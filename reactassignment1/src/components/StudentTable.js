4acf592107ae8a5ee1e309ea2ed077058a93c967
Lasantha Kumara <lasanthakumaraofficial@gmail.com>
2024-11-19 17:20:53 +0000

    created StudentTable component

---
+++ reactassignment1/src/components/StudentTable.js
@@ -0,0 +1,34 @@
+import '../assets/css/compo.css'
+
+export default function StudentTable(props){
+    return(
+        <div className="outerDiv">
+            <div className="leftDiv">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>First Name</td>
+                            <td>Last Name</td>
+                            <td>Course</td>
+                            <td>Country</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            props.students.map(student=>
+                                <tr>
+                                    <td>{student.firstname}</td>
+                                    <td>{student.lastname}</td>
+                                    <td>{student.course}</td>
+                                    <td>{student.address.country}</td>
+                                </tr>
+
+                            )
+                        }
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    )
+}
