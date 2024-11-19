d37a91f8de10cb2a2ebef9f9a1ac5673bc255e5c
Lasantha Kumara <lasanthakumaraofficial@gmail.com>
2024-11-19 17:47:19 +0000

    created profile component

---
+++ reactassignment1/src/components/profile.js
@@ -0,0 +1,13 @@
+export default function(){
+    return(
+        <>
+            <table>
+                <tr>
+                    <td rowSpan={7}>
+                        <img src={require("../assets/img/1.jpg")} alt="profile picture" />
+                    </td>
+                </tr>
+            </table>
+        </>
+    )
+}
